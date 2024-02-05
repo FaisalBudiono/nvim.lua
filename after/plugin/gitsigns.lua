456410cac45a634b74e113a3d017e28d59fe189d
Faisal Budiono <ucul.project@gmail.com>
2024-02-05 23:07:51 +0700

    feat(git): add shortcut for git blame-y

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -4,3 +4,11 @@ gs.setup()
 
 vim.keymap.set('n', '<leader>gb', function() gs.blame_line{full=true} end)
 
+vim.keymap.set('n', '<leader>ghh', gs.preview_hunk)
+vim.keymap.set('n', '<leader>ghn', gs.next_hunk)
+vim.keymap.set('n', '<leader>ghp', gs.prev_hunk)
+
+vim.keymap.set('n', '<leader>gsd', gs.diffthis)
+vim.keymap.set('v', '<leader>gsa', function()
+    gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')}
+end)
