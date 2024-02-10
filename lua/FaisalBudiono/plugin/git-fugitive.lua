f5d975ab93919f78c95bd4d4a317d04da03b3723
Faisal Budiono <ucul.project@gmail.com>
2024-02-10 19:40:31 +0700

    ref: change lua file names for git plugins

---
+++ lua/FaisalBudiono/plugin/git-fugitive.lua
@@ -0,0 +1,15 @@
+return {
+    "tpope/vim-fugitive",
+    lazy = true,
+    keys = {
+        {
+            "<leader>ggg", 
+            function()
+                vim.cmd("tabnew")
+                vim.cmd("Git")
+                vim.cmd("wincmd k")
+                vim.cmd("q")
+            end,
+        },
+    },
+}
