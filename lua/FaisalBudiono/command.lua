1d1db94cb25100949f35b549b77ce3022b752ce6
Faisal Budiono <ucul.project@gmail.com>
2024-02-10 20:04:15 +0700

    feat(cmd): remove unused commands

--- lua/FaisalBudiono/command.lua
+++ lua/FaisalBudiono/command.lua
@@ -1,18 +1,3 @@
-local function cgg()
-    vim.cmd('Git log --oneline --pretty=oneline --graph')
-end
-vim.api.nvim_create_user_command('Cgg', cgg, { nargs='?' })
-
-local function cggv()
-    vim.cmd('Git log --pretty=full --graph')
-end
-vim.api.nvim_create_user_command('Cggv', cggv, { nargs='?' })
-
-local function cggvv()
-    vim.cmd('Git log --pretty=fuller --graph')
-end
-vim.api.nvim_create_user_command('Cggvv', cggvv, { nargs='?' })
-
 local function ccfp()
     vim.cmd('let @+ = expand("%:~:.")')
 end
