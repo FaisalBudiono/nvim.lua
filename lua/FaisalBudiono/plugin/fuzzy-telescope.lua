ec0092a658d76c8cb14398bcbbc3ceb1bfcf6d9d
Faisal Budiono <ucul.project@gmail.com>
2024-02-13 15:17:02 +0700

    feat(telescope): make it able to find dotfile

--- lua/FaisalBudiono/plugin/fuzzy-telescope.lua
+++ lua/FaisalBudiono/plugin/fuzzy-telescope.lua
@@ -6,19 +6,19 @@ return {
         "nvim-lua/plenary.nvim",
         "BurntSushi/ripgrep",
         {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
         },
     },
     keys = {
-        { "<C-p>", "<cmd>lua require('telescope.builtin').git_files()<cr>"},
-        { "<leader>ps", "<cmd>lua require('telescope.builtin').live_grep()<cr>"},
-        { "<leader>pf", "<cmd>lua require('telescope.builtin').find_files()<cr>"},
-        { "<leader><Bslash>", "<cmd>lua require('telescope.builtin').keymaps()<cr>"},
-        { "<leader>ghb", "<cmd>lua require('telescope.builtin').git_bcommits()<cr>"},
-        { "<leader>ghh", "<cmd>lua require('telescope.builtin').git_commits()<cr>"},
+        { "<C-p>", "<cmd>lua require('telescope.builtin').git_files()<cr>" },
+        { "<leader>ps", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
+        { "<leader>pf", "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>" },
+        { "<leader><Bslash>", "<cmd>lua require('telescope.builtin').keymaps()<cr>" },
+        { "<leader>ghb", "<cmd>lua require('telescope.builtin').git_bcommits()<cr>" },
+        { "<leader>ghh", "<cmd>lua require('telescope.builtin').git_commits()<cr>" },
     },
     config = function()
         require("telescope").load_extension("fzf")
-    end
+    end,
 }
