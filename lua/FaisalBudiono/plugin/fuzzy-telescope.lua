ef67fe4d1a6be645e235923ffa525e21c78aa184
Faisal Budiono <ucul.project@gmail.com>
2024-02-17 03:48:23 +0700

    feat(telescope): add grep_string

--- lua/FaisalBudiono/plugin/fuzzy-telescope.lua
+++ lua/FaisalBudiono/plugin/fuzzy-telescope.lua
@@ -13,7 +13,13 @@ return {
     },
     keys = {
         { "<C-p>", "<cmd>lua require('telescope.builtin').git_files()<cr>" },
-        { "<leader>ps", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
+        {
+            "<leader>ps",
+            function()
+                require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+            end,
+        },
+        { "<leader>pS", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
         { "<leader>pf", "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>" },
         { "<leader><Bslash>", "<cmd>lua require('telescope.builtin').keymaps()<cr>" },
         { "<leader>ghb", "<cmd>lua require('telescope.builtin').git_bcommits()<cr>" },
