ee78d10053e1db8058e537a08449febce9b48dd7
Faisal Budiono <ucul.project@gmail.com>
2024-02-21 18:34:22 +0700

    feat(ui): change harpoon colorscheme like catppucin

--- lua/FaisalBudiono/plugin/colorscheme-catppucin.lua
+++ lua/FaisalBudiono/plugin/colorscheme-catppucin.lua
@@ -11,6 +11,7 @@ return {
             integrations = {
                 -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
                 gitsigns = true,
+                harpoon = true,
                 barbecue = {
                     dim_dirname = true, -- directory name is dimmed by default
                     bold_basename = true,
