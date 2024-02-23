d348139a747cac8c3ecc48459deade11104159f6
Faisal Budiono <ucul.project@gmail.com>
2024-02-23 15:56:52 +0700

    feat(lualine): remove filetype section since it already on barbeque

--- lua/FaisalBudiono/plugin/lualine.lua
+++ lua/FaisalBudiono/plugin/lualine.lua
@@ -13,7 +13,7 @@ return {
                 sections = {
                     lualine_a = { "mode" },
                     lualine_b = { "branch", "diff", "diagnostics" },
-                    lualine_c = { "filename" },
+                    lualine_c = {},
                     lualine_x = { "encoding", "fileformat", "filetype" },
 
                     lualine_y = {
