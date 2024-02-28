22255d2ae0ea782cecfed242333f9fb8030f9e77
Faisal Budiono <ucul.project@gmail.com>
2024-02-28 11:57:26 +0700

    fix(lsp): use default composer.json schema

--- lua/FaisalBudiono/plugin/lsp/lang/jsonls.lua
+++ lua/FaisalBudiono/plugin/lsp/lang/jsonls.lua
@@ -4,16 +4,7 @@ return {
     autostart = false,
     settings = {
         json = {
-            schemas = schema_store.json.schemas({
-                replace = {
-                    ["composer.json"] = {
-                        description = "Custom composer.json",
-                        fileMatch = { "composer.json" },
-                        name = "composer.json",
-                        url = "https://gist.githubusercontent.com/FaisalBudiono/3392f25dda51dc837d607e21d8be1a59/raw/de381b147ae491d82418854027b7635b9ac1b508/composer-schema.json",
-                    },
-                },
-            }),
+            schemas = schema_store.json.schemas(),
             validate = { enable = true },
         },
     },
