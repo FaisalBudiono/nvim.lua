81d487467c51ff9da14f7243cdbb930f3b1b352d
Faisal Budiono <ucul.project@gmail.com>
2024-03-01 10:50:09 +0700

    feat(markdown): add remap for markdown preview

--- lua/FaisalBudiono/plugin/markdown-viewer.lua
+++ lua/FaisalBudiono/plugin/markdown-viewer.lua
@@ -1,6 +1,15 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    keys = {
+        {
+            "<leader>mp",
+            ':MarkdownPreview<CR>',
+            noremap = true,
+            silent = true,
+            desc = "Run markdown preview",
+        },
+    },
     ft = { "markdown" },
     build = function()
         vim.fn["mkdp#util#install"]()
