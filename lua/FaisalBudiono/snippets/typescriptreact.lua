9d9cfa305b102ff175482c66df2ee175c9b9afe4
Faisal Budiono <ucul.project@gmail.com>
2024-05-23 05:57:02 +0700

    feat(snippet): add custate

--- lua/FaisalBudiono/snippets/typescriptreact.lua
+++ lua/FaisalBudiono/snippets/typescriptreact.lua
@@ -1,6 +1,7 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
+local t = ls.text_node
 local f = ls.function_node
 local fmt = require("luasnip.extras.fmt").fmt
 
@@ -15,6 +16,23 @@ console.debug('kambing', {})
             ]],
             {
                 i(0),
+            }
+        )
+    ),
+
+    s(
+        "custate",
+        fmt(
+            [[
+const [{}, set{}] = useState()
+            ]],
+            {
+                i(1, "stateName"),
+                f(function(val)
+                    local stateName = val[1][1]
+
+                    return stateName:sub(1, 1):upper() .. stateName:sub(2, -1)
+                end, { 1 }),
             }
         )
     ),
