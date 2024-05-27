23e4fa0e66dae9398a5073eb2e4e546a3b52ae29
Faisal Budiono <ucul.project@gmail.com>
2024-05-27 11:52:08 +0700

    feat(comment): add JSX comment capabilities

--- lua/FaisalBudiono/plugin/comment.lua
+++ lua/FaisalBudiono/plugin/comment.lua
@@ -1,14 +1,17 @@
 return {
     "numToStr/Comment.nvim",
     event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
     config = function()
         local comment = require("Comment")
 
-        comment.setup()
+        comment.setup({
+            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        })
 
         local ft = require("Comment.ft")
         local filetypes = require("FaisalBudiono.filetype").filetypes
 
-        ft({filetypes["eraser-diagram"]}, "//%s")
+        ft({ filetypes["eraser-diagram"] }, "//%s")
     end,
 }
