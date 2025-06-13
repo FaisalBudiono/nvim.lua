62866b679ad9c47769076cfc0d155ea757e93d79
Faisal Budiono <ucul.project@gmail.com>
2025-06-14 01:11:00 +0700

    fix(telescope): telescope-fzf-native.nvim build is deprecated

--- lua/FaisalBudiono/plugin/fuzzy-telescope.lua
+++ lua/FaisalBudiono/plugin/fuzzy-telescope.lua
@@ -8,7 +8,7 @@ return {
         "BurntSushi/ripgrep",
         {
             "nvim-telescope/telescope-fzf-native.nvim",
-            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+            build = "make",
         },
     },
     keys = {
