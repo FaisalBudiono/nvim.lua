47c718a7ab3cd23b368038ef79978663ac89db94
Faisal Budiono <ucul.project@gmail.com>
2025-06-14 18:55:46 +0700

    fix(lua): remove vim is not defined error
    This is caused by selene

--- lua/FaisalBudiono/plugin/lsp/lang/lua_ls.lua
+++ lua/FaisalBudiono/plugin/lsp/lang/lua_ls.lua
@@ -1,13 +1,19 @@
 return {
     settings = { -- custom settings for lua
         Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
             -- make the language server recognize "vim" global
             diagnostics = {
                 globals = { "vim" },
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
+                library = {
+                    vim.env.VIMRUNTIME .. "/lua",
+                },
             },
         },
     },
